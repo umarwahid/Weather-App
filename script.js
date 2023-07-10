d8e92db599e2590db345245eadfa187868cb1f37
Umar Wahid <66332510+umarwahid@users.noreply.github.com>
2023-07-10 10:11:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+
+
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
+const icon = document.querySelector('.weather-image');
+
+const location_not_found = document.querySelector('.location-not-found');
+const weather_body = document.querySelector('.weather-body');
+
+async function checkWeather(city){
+	const api_key = "7089467baf1548b69b644959230707";
+	const url = `https://api.weatherapi.com/v1/current.json?key=${api_key}&q=${city}&aqi=no`
+
+	const weather_data = await fetch(`${url}`)
+	.then(response => response.json())
+
+	if (weather_data.error && weather_data.error.code === 1006) {
+		location_not_found.style.display = 'flex';
+		weather_body.style.display = 'none';
+		console.log(weather_data.error);
+		return;
+	  }
+	location_not_found.style.display = 'none';
+	weather_body.style.display = 'flex';
+
+	temperature.innerHTML = `${weather_data.current.temp_c}°C`;
+	description.innerHTML = `${weather_data.current.condition.text}`;
+	humidity.innerHTML = `${weather_data.current.humidity}%`;
+	wind_speed.innerHTML = `${weather_data.current.wind_kph}kp/H`;
+	icon.src = `${weather_data.current.condition.icon}`;
+	// console.log(weather_data.current.condition.text);
+
+	// switch(weather_data.current.condition.text){
+	// 	case 'Clouds':
+	// 		weather_img.src = "/images/clouds.png"
+	// 		break;
+	// 	case 'Clear':
+	// 		weather_img.src = "/images/clear.png"
+	// 		break;
+	// 	case 'Drizzle':
+	// 		weather_img.src = "/images/drizzle.png"
+	// 		break;
+	// 	case 'Humidity':
+	// 		weather_img.src = "/images/humidity.png"
+	// 		break;
+	// 	case 'Mist':
+	// 		weather_img.src = "/images/mist.png"
+	// 		break;
+	// 	case 'Rain':
+	// 		weather_img.src = "/images/rain.png"
+	// 		break;
+	// 	case 'Snow':
+	// 		weather_img.src = "/images/snow.png"
+	// 		break;
+	// 	case 'Wind':
+	// 		weather_img.src = "/images/wind.png"
+	// 		break;
+	// }
+}
+
+searchBtn.addEventListener('click', (e)=>{
+	e.preventDefault();
+	checkWeather(inputBox.value);
+})
